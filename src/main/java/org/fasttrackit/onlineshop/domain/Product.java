47306462c20f51f715b954788ed27b4fb5bdb5e2
Florin Jurchis <jurchis@gmail.com>
2019-11-18 20:11:39 +0200

    Added product entity and DB configuration

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/Product.java
@@ -0,0 +1,85 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+//Using JavaX from suggestions
+//Spring boot will create the DB Table for this class using below details by @Entity
+@Entity
+public class Product {
+
+    //With @Id we mention the fact that the @Id is main key and @GeneratedValue all will refer to only @GeneratedValue
+    @Id
+    @GeneratedValue
+    private long id;
+    private String description;
+    @NotNull
+    private String name;
+    @NotNull
+    private double price;
+    @NotNull
+    private int quantity;
+    private String imageUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
