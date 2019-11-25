272262eb92b729d030fcafdef212dfcc4454a43e
Florin Jurchis <jurchis@gmail.com>
2019-11-25 21:31:20 +0200

    added update product and delete methods

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -6,6 +6,7 @@ import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -47,5 +48,22 @@ public class ProductService {
                 //lambda expression
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Product " + id + " does not exist."));
+    }
+
+    public Product updateProduct(long id, SaveProductRequest request) {
+        LOGGER.info("Updating product {}: {}", id, request);
+
+        Product retrievedProduct = getProduct(id);
+
+        //from spring framework
+        BeanUtils.copyProperties(request, retrievedProduct);
+
+        return productRepository.save(retrievedProduct);
+    }
+
+    public void deleteProduct(long id) {
+        LOGGER.info("Deleting product {}:",id);
+        productRepository.deleteById(id);
+        LOGGER.info("Product id: {} has been deleted",id);
     }
 }
